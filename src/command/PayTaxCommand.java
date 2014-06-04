be626216ce518b87a6e0070c31e1d1469c00a507
Burcu S. <burcu.sarikaya@ozu.edu.tr>
2014-06-04 23:08:55 +1200

    payRent & payTax added

--- src/command/PayTaxCommand.java
+++ src/command/PayTaxCommand.java
@@ -2,12 +2,12 @@ package command;
 
 import controller.Controller;
 
-public class PayCommand implements Command {
+public class PayTaxCommand implements Command {
 
 	@Override
 	public void execute(Controller controller) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 }
